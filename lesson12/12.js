34f686761bf908113627f1a3db8308e4a04a23dc
ssivukha <stas3108@gmail.com>
2019-02-07 12:05:21 +0200

    add 11 and 12

---
+++ lesson12/12.js
@@ -0,0 +1,152 @@
+const showButton = document.getElementById('show-button');
+const filterInput = document.getElementById('filter-coctails');
+const listElement = document.getElementById('cocktail-list');
+const isAlcoholFilter = document.querySelector('#is-alcohol');
+const isLongFilter = document.querySelector('#is-long');
+
+const orderListElement = document.querySelector('.order-list');
+const orderButton = document.querySelector('.button-order')
+
+class Cocktail {
+    constructor (name, ingredients, isAlcohol, type) {
+        this.name = name; // instance property
+        this.ingredients = ingredients;
+        this.isAlcohol = isAlcohol;
+        this.type = type;
+    }
+    getPrice() { // methods of prototype
+        return this.ingredients.reduce(function (sum, ingredient) {
+            return sum+ingredient.price
+        }, 0)
+    }
+}
+
+class CocktailsList {
+    constructor () {
+        this.list = [] // model
+        this.filteredList = []; // view model 
+
+        this.filters = { // view model
+            isAlcohol: true,
+            isLong: true,
+            cocktailValue: ''
+        }
+    }
+
+    setFilterValue (value, type) {
+        this.filters[type] = value;
+    }
+
+    add(cocktail) {
+        this.list.push(cocktail)
+    }
+
+    remove(cocktail) {
+        let index = this.list.indexOf(cocktail);
+        if (index != -1) {
+           return this.list.splice(index, 1)
+        }
+        return list;
+    }
+
+    getAll() {
+        return this.list;
+    }
+
+    showAlcohol() {
+        return this.list.filter(item => item.isAlcohol === true)
+    }
+
+    showNonAlcohol() {
+        return this.list.filter(item => item.isAlcohol === false)
+    }
+
+    applyFilters() {
+        this.filteredList = this.list.filter(item => {
+            return item.isAlcohol === this.filters.isAlcohol
+             && (item.type === 'long' && this.filters.isLong || item.type === 'shot' && !this.filters.isLong  )
+             && (this.filters.cocktailValue === '' || item.name.indexOf(this.filters.cocktailValue) === 0 )
+        });     
+    }
+  
+    render() {
+        this.applyFilters(); // change this.list
+        let fragment = document.createDocumentFragment();
+        
+        this.filteredList.forEach(function (item) {
+            let cocktailItem = document.createElement('div');
+            let orderButton = document.createElement('button');
+
+            cocktailItem.innerText = item.name;
+            cocktailItem.className = 'cocktail';
+            fragment.appendChild(cocktailItem);
+
+
+            cocktailItem.appendChild(orderButton);
+            orderButton.appendChild(document.createTextNode('buy'));
+            orderButton.className = "button-order";
+        })
+        return fragment;
+    }
+}
+
+class Order {
+    constructor () {
+        this.orderList = [];
+    }
+
+    totalPrice() { // methods of prototype
+        return this.items.reduce((sum, item) => item.price+sum, 0)
+    }
+
+    renderOrder() {
+        let fragment = document.createDocumentFragment();
+        
+        this.orderList.forEach(function (item) {
+            let cocktailItem = document.createElement('div');
+            
+            cocktailItem.innerText = `<div>${item.name}</div> + <div>${item.name}</div> + <div>${item.price}</div> + <div>${this.totalPrice()}</div>`;
+            cocktailItem.className = 'cocktail-ordered';
+
+            fragment.appendChild(cocktailItem);
+        })
+        return fragment;
+    }
+
+}
+
+
+let list = new CocktailsList();
+list.add(new Cocktail('margarita', [{name: 'tequila', price: 5},{name: 'lime', price: 3} ], true, 'long'))
+list.add(new Cocktail('old fashioned', [{name: 'wiskey', price: 6},{name: 'bitter', price: 3} ], true, 'long'))
+list.add(new Cocktail('bloody mary', [{name: 'vodka', price: 4},{name: 'juice', price: 2} ], true, 'shot'))
+list.add(new Cocktail('orange juice', [{name: 'juice', price: 3}], false, 'long'))
+
+
+const showList = function () {
+    listElement.innerHTML = '';
+    listElement.appendChild(list.render())
+}
+listElement.appendChild(list.render())
+
+
+function filterHandler () {
+    let value = this.type === "checkbox" ? this.checked : this.value;
+    list.setFilterValue(value, this.name);
+    listElement.innerHTML = '';
+    listElement.appendChild(list.render())
+}
+
+isAlcoholFilter.addEventListener('change', filterHandler);
+isLongFilter.addEventListener('change', filterHandler);
+filterInput.addEventListener('input', filterHandler);
+
+
+
+
+let orderList = new Order();
+
+const addOrder = function () {
+    
+}
+orderButton.addEventListener('click', addOrder);
